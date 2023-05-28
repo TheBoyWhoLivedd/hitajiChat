42fa79b7ae6b702d95b7aaadbf581123a3e9f2c6
TheBoyWhoLivedd <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-05-28 12:13:24 +0300

    Update route.ts

--- src/app/api/chats/[userId]/[chatId]/route.ts
+++ src/app/api/chats/[userId]/[chatId]/route.ts
@@ -50,8 +50,8 @@ async function initPinecone() {
 if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error("Missing Pinecone index name in .env file");
 }
-const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? "";
-const PINECONE_NAME_SPACE = process.env.PINECONE_NAME_SPACE ?? "trial-three"; //namespace is optional for your vectors
+const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? "hitaji-chat";
+// const PINECONE_NAME_SPACE = process.env.PINECONE_NAME_SPACE ?? "trial-three"; //namespace is optional for your vectors
 
 if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error("Missing Pinecone index name in .env file");
